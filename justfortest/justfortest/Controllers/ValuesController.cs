527bff9bbd35a20f6178600e22f1612065a620bf
NateJiang0928 <natejiang9527@github.com>
2024-11-25 21:00:38 +0800

    just for test step 1

---
+++ justfortest/justfortest/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace justfortest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string get() 
+        {
+            return "good";
+        }
+    }
+}
